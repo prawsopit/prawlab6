83aae936eb1f6bf4c0bcb70cd62cb3863061edd4
prawsopit <prawsopit@gmail.com>
2022-12-09 16:21:26 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
+    int numbers = 1 ;
+     int odd =0; 
+     int even =0;
+    
+while (numbers !=0) {
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+     cin >> numbers ;
+     if(numbers == 0) break;
+     if(numbers%2 ==0) {
+        even ++ ;
+     }
+      else {
+       odd ++ ;
+      }
+}
+
+
+    cout << "#Even numbers =  " << even;
+    cout << "\n#Odd numbers = " << odd;
     return 0;
 }
